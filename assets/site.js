c664b3631d048264e1f10880a499357252a2814d
Jan Svaty <jan.svaty@gmail.com>
2026-07-26 12:54:35 +1200

    Update portfolio

--- assets/site.js
+++ assets/site.js
@@ -5,7 +5,7 @@ if (introOverlay && !document.documentElement.classList.contains('intro-skip')) 
   setTimeout(() => {
     introOverlay.classList.add('is-leaving');
     introOverlay.addEventListener('animationend', () => introOverlay.remove(), { once: true });
-  }, 2400);
+  }, 1800);
 }
 
 // Dark / light mode toggle
